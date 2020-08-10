6b471ff44ee94be390e772d104483df5e5ec7a25
laura.cavaliere@iit.it <laura.cavaliere@iit.it>
2020-08-10 17:49:27 +0200

    Specifying the python version in createGUI

--- gui/createGUI.sh
+++ gui/createGUI.sh
@@ -19,23 +19,26 @@ fi
 # if we already have venv, we don't need to rerun it
 if [ ! -d "venv" ]
 then
-    python3 -m venv venv
+    python3.6 -m venv venv
 fi
 
 # initialize our python virtual environment
 source venv/bin/activate
 
 # install python dependencies (QT for graphical interface, watchdog for file monitors, fbs to install the GUI)
-pip install -q fbs PyQt5==5.9.2 watchdog
+
+#pip3 install pyqt5 
+#pip install -q fbs  watchdog
+pip install fbs PyQt5==5.9.2 watchdog
 
 # start the project. We pipe the name of the app, "appGUI" into the command to avoid user prompts. The output is quite verbose so we make it run silent.
-(echo appGUI ; echo '' ; echo '') | fbs startproject > /dev/null
+(echo appGUI ; echo '' ; echo '') | fbs startproject #> /dev/null
 
 # copy the actual code in main.py into the project
 cp main.py ./src/main/python/main.py
 
 # compile our GUI. The output is quite verbose so we make it run silent.
-fbs freeze > /dev/null
+fbs freeze #> /dev/null
 
 # copy application images and configuration files (e.g. buttons) to the GUI folder
 cp ../demos/$APPSAWAY_APP_NAME/gui/gui_conf.ini ./target/appGUI/
