88f8c51f8603c72fb6e0946615f03d1490b1f131
AlexAntn <alexandre.antunes@plymouth.ac.uk>
2020-08-12 10:28:36 +0200

    changes in createGUI to set the python version ONLY to install the GUI, then sets it back to default

--- gui/createGUI.sh
+++ gui/createGUI.sh
@@ -4,6 +4,19 @@
 
 source ../scripts/appsAway_setEnvironment.local.sh
 
+
+# first we check which python is being used in the machine and store that path
+PYTHON_VERSION=$(python3 -V) # python 3.6.2
+MAJOR_MINOR=${PYTHON_VERSION:7:3}
+ORIGIN_PYTHON_PATH=$(which python$MAJOR_MINOR)    # /usr/bin/python3.6
+
+# now we update which python to use for installing the gui
+echo $APPSAWAY_USER_PASSWORD | sudo -S add-apt-repository ppa:deadsnakes/ppa
+echo $APPSAWAY_USER_PASSWORD | sudo -S apt-get update
+echo $APPSAWAY_USER_PASSWORD | sudo -S apt-get install python3.6 python3.6-dev python3.6-venv python3.6-pip
+echo $APPSAWAY_USER_PASSWORD | sudo -S update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+echo $APPSAWAY_USER_PASSWORD | sudo -S update-alternatives --set python3 /usr/bin/python3.6
+
 # checks if there is a previous installation - if yes, it deletes it
 if [ -d "src" ]
 then
@@ -39,3 +52,11 @@ cp main.py ./src/main/python/main.py
 
 # compile our GUI. The output is quite verbose so we make it run silent.
 fbs freeze #> /dev/null
+
+
+echo $APPSAWAY_USER_PASSWORD | sudo -S update-alternatives --remove python3 /usr/bin/python3.6
+echo $APPSAWAY_USER_PASSWORD | sudo -S update-alternatives --install /usr/bin/python3 python3 $ORIGIN_PYTHON_PATH 2
+echo $APPSAWAY_USER_PASSWORD | sudo -S update-alternatives --set python3 $ORIGIN_PYTHON_PATH
+
+
+
