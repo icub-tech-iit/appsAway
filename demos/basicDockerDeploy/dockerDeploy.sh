ff94ef731da86c102641209a080bd05f3c3ef981
Vadim Tikhanoff <vtikha@gmail.com>
2020-09-02 17:45:41 +0200

    removing duplicate ansible setup

--- demos/basicDockerDeploy/dockerDeploy.sh
+++ demos/basicDockerDeploy/dockerDeploy.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-cd ${HOME}/teamcode/appsAway/scripts/ansible_setup
-./setup_hosts_ini.sh
-echo "Checking installation with Ansible..."
-make prepare
-echo "Done checking installation with Ansible..."
-cd ..
-
 docker run  -it --network host icubteamcode/superbuild:master-unstable_sources bash
