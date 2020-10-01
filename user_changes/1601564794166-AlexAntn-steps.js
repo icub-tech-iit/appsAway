aae3797ed8e2171e3ec8d79cefcfcb7a4b7ff5c9
GitHub Action <actions@github.com>
2020-10-01 15:46:17 +0000

    Merged user changes in appsAway

---
+++ user_changes/1601564794166-AlexAntn-steps.js
@@ -0,0 +1,7 @@
+//AlexAntn
+//db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19cc")},{$set: { summary: "This application will launch the interactive object learning for your robot. This is still under development and will be available shortly
+
+THIS IS A TEST"}});
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19cc")},{$set: { summary: "This application will launch the interactive object learning for your robot. This is still under development and will be available shortly
+
+THIS IS A TEST!"}});
