6685145d26bd2bd4984630a34d5ac7ae9cb05f93
GitHub Action <actions@github.com>
2020-10-13 15:10:59 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee8")},{$set: { title: "Human Sensing",description: "Deploy the human sensing application on your cluster",updated: "Jul 2020"}});
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee8")},{$set: { title: "Face Landmarks and Pose Estimation",description: "Deploy the Face Landmarks and Pose Estimation application on your cluster",updated: "Oct 2020"}});
