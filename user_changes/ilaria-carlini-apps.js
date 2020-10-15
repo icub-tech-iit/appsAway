3ff77ab523d802fee87926b0e6063a947ee99aa3
GitHub Action <actions@github.com>
2020-10-15 07:15:41 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee4")},{$set: { title: "Object Detection application"}});
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee4")},{$set: { title: "Object Detection application test"}});
