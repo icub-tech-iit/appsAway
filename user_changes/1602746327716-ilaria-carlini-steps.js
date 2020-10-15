6f80bc48a991c1c72f6625b4370a327510e763f7
GitHub Action <actions@github.com>
2020-10-15 07:20:40 +0000

    Merged user changes in appsAway

---
+++ user_changes/1602746327716-ilaria-carlini-steps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19cd")},{$set: { summary: "This application will launch the Object Detection application for your robot. This is still under development and will be available shortly"}});
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19cd")},{$set: { summary: "This application will launch the Object Detection application for your robot. This is still under development and will be available shortly TEST"}});
