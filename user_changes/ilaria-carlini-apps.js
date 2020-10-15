d52e4a175b0e7a36aafa6885a4749fd789019a67
GitHub Action <actions@github.com>
2020-10-15 09:25:41 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fede")},{$set: { title: "Gazebo Simulation Deployment",description: "Deploy a pre configured Docker including Gazebo simulator for your system"}});
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fede")},{$set: { title: "Gazebo Simulation Deployment TEST",description: "Deploy a pre configured Docker including Gazebo simulator for your system TEST"}});
