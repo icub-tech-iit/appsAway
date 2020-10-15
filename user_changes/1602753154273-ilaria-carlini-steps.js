adff5c1050e9f32361e3b32e50fda321b3f15a3b
GitHub Action <actions@github.com>
2020-10-15 09:15:45 +0000

    Merged user changes in appsAway

---
+++ user_changes/1602753154273-ilaria-carlini-steps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19c3")},{$set: { media_content: ["assets/images/gazeboSimulator/gazebo-simulator.png"]}});
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19c3")},{$set: { media_content: ["assets/images/gazeboSimulator/gazebo-simulator.png","assets/images/uploads/1602753154273-robotGazebo-mediacontent0.png"]}});
