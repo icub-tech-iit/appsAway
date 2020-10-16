e9eb823b33ec85993837e798374b4ef7fe355c0e
GitHub Action <actions@github.com>
2020-10-16 07:23:16 +0000

    Merged user changes in appsAway

---
+++ user_changes/AlexAntn-apps.js
@@ -0,0 +1,3 @@
+//AlexAntn
+//db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee8")},{$set: { description: "Deploy the human sensing application on your cluster"}});
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee8")},{$set: { description: "Deploy the Face Landmarks and Pose Detection application on your cluster"}});
