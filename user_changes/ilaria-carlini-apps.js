ce881ed448c80af0fe8edb7ab19e72a6b6b6a0c2
GitHub Action <actions@github.com>
2020-10-16 16:19:53 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//&<h3>Field changed in app: <ins>Speech-to-text TEST</ins></h3><ul><li>title: <del>Google s</del><b>S</b>peech-to-text<b> TEST</b></li><li>description: Deploy the <del>Google</del><b>TEST</b> speech-to-text application on your cluster<b> TEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f8581b48211d743b5898c30")},{$set: { title: "Speech-to-text TEST",description: "Deploy the TEST speech-to-text application on your cluster TEST"}});
