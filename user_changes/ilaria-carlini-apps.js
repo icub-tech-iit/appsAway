7d6eb3b0d2cda8fbcf86e2dcbe5045f85d6a65e3
GitHub Action <actions@github.com>
2020-10-16 15:59:04 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//&<h3>Field changed in object_id: <ins>5f4375f118a3dc019958fedf</ins></h3><ul><li>title: yarprobotinterface <del>Deployment</del><b>TEST</b></li><li>description: Deploy a clean yarprobotinterface application for your robot<b> TEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fedf")},{$set: { title: "yarprobotinterface \"TEST\"",description: "Deploy a clean yarprobotinterface application for your robot TEST\""}});
