2ad1c970259bddb6ea1f0973cbfb2cb05e1d082e
GitHub Action <actions@github.com>
2020-10-16 08:14:46 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fedd")},{$set: { title: "Basic Docker Deployment"}});
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fedd")},{$set: { title: "Basic Docker Deployment432"}});
