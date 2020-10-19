0262de0dd1cc95f408b51be937ea325be1fae359
GitHub Action <actions@github.com>
2020-10-19 07:40:42 +0000

    Merged user changes in appsAway

---
+++ user_changes/ilaria-carlini-apps.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//&<h3>Field changed in app: <ins>Google Dialog</ins></h3><ul><li>description: Deploy the Google Dialog application on your cluster<b> TEST</b></li><li>category: <del>Applica</del><b>Sys</b>t<del>ion</del><b>em</b></li><li>platform: [iCub,<del>iCub3</del><b>R1</b>]</li></ul>
+db.apps.update ({_id: ObjectId("5f8051f31d150f9ee5746102")},{$set: { description: "Deploy the Google Dialog application on your cluster TEST",category: "System",platform: ["iCub","R1"]}});
