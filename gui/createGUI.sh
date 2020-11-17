90d95f15bd819fb51ece95748c9d5c48b0f3c65e
AlexAntn <alexandre.antunes@plymouth.ac.uk>
2020-11-17 15:35:58 +0100

    changed the GUI creation script to not be so verbose

--- gui/createGUI.sh
+++ gui/createGUI.sh
@@ -13,22 +13,29 @@ ORIGIN_PYTHON_PATH=$(which python$MAJOR_MINOR)    # /usr/bin/python3.6
 echo ""
 read -p "Insert your password: " -s password
 echo ""
+echo -ne "[              ] installing GUI...\r"
 
 # now we update which python to use for installing the gui
 os=`uname -s`
 if [ "$os" = "Darwin" ]
 then
-  echo ${password} | HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv
-  echo ${password} | env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install -f 3.6.1
-  PYTHON_VERSION=$( pyenv global ) 
-  echo ${password} | pyenv global 3.6.1
+  echo ${password} | HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv > /dev/null 
+  echo ${password} | env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install -f 3.6.1 > /dev/null 
+  PYTHON_VERSION=$( pyenv global )
+  echo ${password} | pyenv global 3.6.1 > /dev/null 
 else
-  echo ${password} | sudo -S add-apt-repository ppa:deadsnakes/ppa
-  echo ${password} | sudo -S apt-get -y update
-  echo ${password} | sudo -S apt-get -y install python3.6 python3.6-dev python3.6-venv
-  echo ${password} | sudo -S update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-  echo ${password} | sudo -S update-alternatives --set python3 /usr/bin/python3.6
-  echo ${password} | sudo -S apt -y install python3-pip
+  echo ${password} | sudo -S add-apt-repository ppa:deadsnakes/ppa > /dev/null 2>&1
+  echo -ne "[.             ] installing GUI...\r"
+  echo ${password} | sudo -S apt-get -y update > /dev/null 2>&1
+  echo -ne "[..            ] installing GUI...\r"
+  echo ${password} | sudo -S apt-get -y install python3.6 python3.6-dev python3.6-venv > /dev/null 2>&1
+  echo -ne "[...           ] installing GUI...\r"
+  echo ${password} | sudo -S update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 > /dev/null 2>&1
+  echo -ne "[....          ] installing GUI...\r"
+  echo ${password} | sudo -S update-alternatives --set python3 /usr/bin/python3.6 > /dev/null 2>&1
+  echo -ne "[.....         ] installing GUI...\r"
+  echo ${password} | sudo -S apt -y install python3-pip > /dev/null 2>&1
+  echo -ne "[......        ] installing GUI...\r"
 fi
 
 
@@ -47,35 +54,44 @@ fi
 # if we already have venv, we don't need to rerun it
 if [ ! -d "venv" ]
 then
-    python3.6 -m venv venv
+    python3.6 -m venv venv > /dev/null 2>&1
 fi
+echo -ne "[.......       ] installing GUI...\r"
 
 # initialize our python virtual environment
 source venv/bin/activate
+echo -ne "[........      ] installing GUI...\r"
 
 # install python dependencies (QT for graphical interface, watchdog for file monitors, fbs to install the GUI)
 
 #pip3 install pyqt5 
 #pip install -q fbs  watchdog
-pip install fbs pygame PyQt5==5.9.2 watchdog
+pip install fbs pygame PyQt5==5.9.2 watchdog > /dev/null 2>&1
+echo -ne "[.........     ] installing GUI...\r"
 
 # start the project. We pipe the name of the app, "appGUI" into the command to avoid user prompts. The output is quite verbose so we make it run silent.
-(echo appGUI ; echo '' ; echo '') | fbs startproject #> /dev/null
+(echo appGUI ; echo '' ; echo '') | fbs startproject > /dev/null 2>&1 #> /dev/null > /dev/null 
+echo -ne "[..........    ] installing GUI...\r"
 
 # copy the actual code in main.py into the project
 cp main.py ./src/main/python/main.py
+echo -ne "[...........   ] installing GUI...\r"
 
 # compile our GUI. The output is quite verbose so we make it run silent.
-fbs freeze #> /dev/null
+fbs freeze > /dev/null 2>&1 #> /dev/null
+echo -ne "[............  ] installing GUI...\r"
 
 
 if [ "$os" = "Darwin" ]
 then
-  echo ${password} | pyenv global $PYTHON_VERSION
+  echo ${password} | pyenv global $PYTHON_VERSION > /dev/null 2>&1
 else
-  echo ${password} | sudo -S update-alternatives --remove python3 /usr/bin/python3.6
-  echo ${password} | sudo -S update-alternatives --install /usr/bin/python3 python3 $ORIGIN_PYTHON_PATH 2
-  echo ${password} | sudo -S update-alternatives --set python3 $ORIGIN_PYTHON_PATH
+  echo ${password} | sudo -S update-alternatives --remove python3 /usr/bin/python3.6 > /dev/null 2>&1
+  echo -ne "[.............  ] installing GUI...\r"
+  echo ${password} | sudo -S update-alternatives --install /usr/bin/python3 python3 $ORIGIN_PYTHON_PATH 2 > /dev/null 2>&1
+  echo -ne "[.............. ] installing GUI...\r"
+  echo ${password} | sudo -S update-alternatives --set python3 $ORIGIN_PYTHON_PATH > /dev/null 2>&1
+  echo "[...............] installing GUI......Done!"
 fi
 
 
