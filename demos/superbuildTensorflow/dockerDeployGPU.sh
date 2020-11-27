d3c3cc8cd24c1b7b3c0a92112863a89050eabf56
Mattia <mattia.sanchioni.dev@gmail.com>
2020-11-27 17:03:05 +0100

    fix script for installing tensorflow and pytorch images, check and install if necessary nvidia-docker2

--- demos/superbuildTensorflow/dockerDeployGPU.sh
+++ demos/superbuildTensorflow/dockerDeployGPU.sh
@@ -1,4 +1,21 @@
 #! /bin/bash
 
+if [ -z "$(dpkg -l | grep nvidia-docker2)" ]; then
+    distribution=$(
+        . /etc/os-release
+        echo $ID$VERSION_ID
+    ) &&
+        curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - &&
+        curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+    sudo apt-get update
+
+    sudo apt-get install -y nvidia-docker2
+
+    sudo systemctl restart docker
+else
+    echo "nvidia-docker2 already installed"
+fi
+
 docker pull icubteamcode/superbuild-tensorflow-gpu:master_master-unstable_sources
-docker run  -it --network host --gpus all icubteamcode/superbuild-tensorflow-gpu:master_master-unstable_sources bash
+docker run -it --network host --gpus all icubteamcode/superbuild-tensorflow-gpu:master_master-unstable_sources bash
