6370a6e1585a098d4bbca944a44bdcd5a25c2b2b
GitHub Action <actions@github.com>
2020-12-09 13:03:07 +0000

    removing user_changes from appsAway

--- user_changes/1607518545348-maggia80-robots.js
+++
@@ -1,3 +0,0 @@
-//maggia80
-//&<h3>Field changed in robot: <ins>iCubWaterloo01</ins></h3><ul><li>gallery: [<b>assets/images/uploads/1607518545348-iCubWaterloo01-gallery0.jpg</b>]</li></ul>
-db.robots.update ({_id: ObjectId("5f1e8af86793b5f3551769c5")},{$set: { gallery: ["assets/images/uploads/1607518545348-iCubWaterloo01-gallery0.jpg"]}});
