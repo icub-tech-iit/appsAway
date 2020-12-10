3e4b98f605cc77e3e6657d887552e0efa7178356
GitHub Action <actions@github.com>
2020-12-10 08:45:38 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607589635877-AlexAntn-steps.js
@@ -0,0 +1,3 @@
+//AlexAntn
+//&<h3>Field changed in app: <ins>Navigation application</ins></h3><ul><li>requirements: [Ssh keyless access on machines allows to run smoothly without inserting password at every command,iCub-head on,Motors on,Robot in the parking position (e.g.: robotinterface not running),Coffee/tea<b>,git (version control system)</b>]</li></ul>
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19cf")},{$set: { requirements: ["Ssh keyless access on machines allows to run smoothly without inserting password at every command","iCub-head on","Motors on","Robot in the parking position (e.g.: robotinterface not running)","Coffee/tea","git (version control system)"]}});
