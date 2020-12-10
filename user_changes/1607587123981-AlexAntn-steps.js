a519f5fe76d5c0061a2bd124392e559b7b70a693
GitHub Action <actions@github.com>
2020-12-10 08:00:42 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607587123981-AlexAntn-steps.js
@@ -0,0 +1,3 @@
+//AlexAntn
+//&<h3>Field changed in app: <ins>Face Landmarks and Pose Detection</ins></h3><ul><li>requirements: [Keyless ssh access: allows to run smoothly without inserting password at each command,iCub-head on,Motors on,Robot in the parking position (e.g.: robotinterface not running),Coffee/tea<b>,git (version control system)</b>]</li></ul>
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19d3")},{$set: { requirements: ["Keyless ssh access: allows to run smoothly without inserting password at each command","iCub-head on","Motors on","Robot in the parking position (e.g.: robotinterface not running)","Coffee/tea","git (version control system)"]}});
