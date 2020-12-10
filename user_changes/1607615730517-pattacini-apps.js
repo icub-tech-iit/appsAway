9f2e888f8217738f50313b98638fb9b230edca45
GitHub Action <actions@github.com>
2020-12-10 16:00:43 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607615730517-pattacini-apps.js
@@ -0,0 +1,3 @@
+//pattacini
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>description: Deploy a pre<del> </del>configured Docker that run<b>s</b> a grasping demonstration for the iCub robot in<del> the</del> Gazebo</li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fe10")},{$set: { description: "Deploy a preconfigured Docker that runs a grasping demonstration for the iCub robot in Gazebo"}});
