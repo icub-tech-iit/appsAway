c048ae120214295500b06628e3b5ea7ac7886142
GitHub Action <actions@github.com>
2020-12-10 08:47:19 +0000

    removing user_changes from appsAway

--- user_changes/1607589677082-AlexAntn-steps.js
+++
@@ -1,3 +0,0 @@
-//AlexAntn
-//&<h3>Field changed in app: <ins>START Q&A</ins></h3><ul><li>requirements: [Ssh keyless access on machines allows to run smoothly without inserting password at every command,Google cloud account activation,Service account key,Coffee/tea<b>,git (version control system)</b>]</li></ul>
-db.steps.update ({_id: ObjectId("5f85b79a958c6d2c08664c35")},{$set: { requirements: ["Ssh keyless access on machines allows to run smoothly without inserting password at every command","Google cloud account activation","Service account key","Coffee/tea","git (version control system)"]}});
