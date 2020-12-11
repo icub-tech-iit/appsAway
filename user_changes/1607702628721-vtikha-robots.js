dee9975a5086fe5457c5d609c8f581152a155c9a
GitHub Action <actions@github.com>
2020-12-11 16:08:00 +0000

    removing user_changes from appsAway

--- user_changes/1607702628721-vtikha-robots.js
+++
@@ -1,3 +0,0 @@
-//vtikha
-//&<h3>Field changed in robot: <ins>CER01</ins></h3><ul><li>gallery: [assets/images/robots/r1_2.jpg,assets/images/robots/r1_3.jpg<b>,assets/images/uploads/1607702628721-CER01-gallery0.png</b>]</li></ul>
-db.robots.update ({_id: ObjectId("5f1e8af86793b5f355176999")},{$set: { gallery: ["assets/images/robots/r1_2.jpg","assets/images/robots/r1_3.jpg","assets/images/uploads/1607702628721-CER01-gallery0.png"]}});
--- user_changes/1607702670869-pattacini-steps.js
+++
@@ -1,7 +0,0 @@
-//pattacini
-//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>summary: Robot simulation is an essential tool in every roboticist's toolbox. A well-designed simulator makes it possible to rapidly test algorithms, design robots, perform regression testing, and train AI system using realistic scenarios. This application launches the Gazebo simulator and allows you to experiment with iCub performing basic object grasping.<b>
-
-Source code available at https://github.com/robotology/icub-gazebo-grasping-sandbox 🌐</b></li></ul>
-db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19db")},{$set: { summary: "Robot simulation is an essential tool in every roboticist's toolbox. A well-designed simulator makes it possible to rapidly test algorithms, design robots, perform regression testing, and train AI system using realistic scenarios. This application launches the Gazebo simulator and allows you to experiment with iCub performing basic object grasping.
-
-Source code available at https://github.com/robotology/icub-gazebo-grasping-sandbox 🌐"}});
