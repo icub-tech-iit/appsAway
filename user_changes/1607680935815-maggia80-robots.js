c3c94ff4f0f96ae717e6f50c3f96d233c93966da
GitHub Action <actions@github.com>
2020-12-11 10:05:44 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607680935815-maggia80-robots.js
@@ -0,0 +1,3 @@
+//maggia80
+//&<h3>Field changed in robot: <ins>iCubWaterloo01</ins></h3><ul><li>gallery: [assets/images/uploads/1607<del>5224</del>6<del>529</del><b>80</b>9<b>35815</b>-iCubWaterloo01-gallery0.jpg]</li></ul>
+db.robots.update ({_id: ObjectId("5f1e8af86793b5f3551769c5")},{$set: { gallery: ["assets/images/uploads/1607680935815-iCubWaterloo01-gallery0.jpg"]}});
