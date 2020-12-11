142c8929d562dd012c347055cf8c755a6e580352
GitHub Action <actions@github.com>
2020-12-11 15:55:45 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607701851273-vtikha-apps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>laboratories: [<b>{name:lab name ,url:lab url }</b>]</li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fe10")},{$set: { laboratories: [{"name":"lab name ","url":"lab url "}]}});
---
+++ user_changes/1607701851273-vtikha-steps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>projects: [<b>{title:project name,url:project url}</b>]</li></ul>
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19db")},{$set: { projects: [{"title":"project name","url":"project url"}]}});
