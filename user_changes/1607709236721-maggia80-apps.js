f48a2bf0f26213bef8a72563642feaec0ed1fc9d
GitHub Action <actions@github.com>
2020-12-11 17:56:46 +0000

    removing user_changes from appsAway

--- user_changes/1607709236721-maggia80-apps.js
+++
@@ -1,3 +0,0 @@
-//maggia80
-//&<h3>Field changed in app: <ins>Camera Pan Calibration</ins></h3><ul><li>laboratories: [<b>{name:iCub Tech,url:https://www.iit.it/research/facilities/icub-tech}</b>]</li></ul>
-db.apps.update ({_id: ObjectId("5fc116d25119091dd4f29bcb")},{$set: { laboratories: [{"name":"iCub Tech","url":"https://www.iit.it/research/facilities/icub-tech"}]}});
