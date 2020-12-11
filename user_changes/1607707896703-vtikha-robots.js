4f4229f850fccb265c23a023d1b2a6c3ea0a4eff
GitHub Action <actions@github.com>
2020-12-11 17:37:13 +0000

    removing user_changes from appsAway

--- user_changes/1607707896703-vtikha-robots.js
+++
@@ -1,3 +0,0 @@
-//vtikha
-//&<h3>Field changed in robot: <ins>CER01</ins></h3><ul><li>img: assets/images/uploads/1607707<del>0</del><b>89</b>6<del>5</del>7<del>71</del><b>03</b>-CER01-img.png</li></ul>
-db.robots.update ({_id: ObjectId("5f1e8af86793b5f355176999")},{$set: { img: "assets/images/uploads/1607707896703-CER01-img.png"}});
