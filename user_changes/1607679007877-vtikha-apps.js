c2e2e0e6902823840f7ef77438782a3ddce0feea
GitHub Action <actions@github.com>
2020-12-11 09:35:45 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607679007877-vtikha-apps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>platform: [<b>iCub,iCub3,R1</b>]</li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fe10")},{$set: { platform: ["iCub","iCub3","R1"]}});
