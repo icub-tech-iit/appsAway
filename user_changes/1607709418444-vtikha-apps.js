464e5d8e4f299a6b8edb45a768130c9415abfe4e
GitHub Action <actions@github.com>
2020-12-11 18:00:40 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607709418444-vtikha-apps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Simulation Deployment VADIM IS THE BEST</ins></h3><ul><li>title: Gazebo Simulation Deployment<b> VADIM IS THE BEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fede")},{$set: { title: "Gazebo Simulation Deployment VADIM IS THE BEST"}});
