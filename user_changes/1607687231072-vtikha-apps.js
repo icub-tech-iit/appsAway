a4d83ae3c4f8dd1807e896a68c3ba5d32bb1cf16
GitHub Action <actions@github.com>
2020-12-11 11:50:41 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607687231072-vtikha-apps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>credits: [<b>Ugo Pattacini - ugo.pattacini@iit.it</b>]</li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fe10")},{$set: { credits: ["Ugo Pattacini - ugo.pattacini@iit.it"]}});
---
+++ user_changes/1607687231072-vtikha-steps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Grasping Sandbox</ins></h3><ul><li>projects: [<b>{title: icub-gazebo-grasping-sandbox,url:https://github.com/robotology/icub-gazebo-grasping-sandbox}</b>]</li></ul>
+db.steps.update ({_id: ObjectId("5f52487b6de3ecd4c51f19db")},{$set: { projects: [{"title":" icub-gazebo-grasping-sandbox","url":"https://github.com/robotology/icub-gazebo-grasping-sandbox"}]}});
