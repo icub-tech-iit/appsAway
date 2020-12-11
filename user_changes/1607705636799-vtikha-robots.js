04fc931cbaf9c51c04661751efcbc0736d348397
GitHub Action <actions@github.com>
2020-12-11 16:55:43 +0000

    Merged user changes in appsAway

---
+++ user_changes/1607705636799-vtikha-robots.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in robot: <ins>CER01</ins></h3><ul><li>img: assets/images/<del>rob</del><b>upl</b>o<del>t</del><b>ad</b>s/<del>r</del>1<b>607705636799-CER01-img</b>.png</li></ul>
+db.robots.update ({_id: ObjectId("5f1e8af86793b5f355176999")},{$set: { img: "assets/images/uploads/1607705636799-CER01-img.png"}});
