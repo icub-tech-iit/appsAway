8e3f2c3237ff35eab6368f04c958a61b1d85e626
GitHub Action <actions@github.com>
2021-03-25 17:18:04 +0000

    removing user_changes from appsAway

--- user_changes/1616691284791-traversaro-robots.js
+++
@@ -1,3 +0,0 @@
-//traversaro
-//&<h3>Field changed in robot manager</h3><br><ul><li>ID: <ins>31</ins><ul><li>os_version: <del>3</del><b>7</b>.0</li><li>os_version_array: <del>3</del><b>7</b>.0</li></ul></li></ul>
-db.robots.update ({id: 31},{$set: { os_version: "7.0", os_version_array: "7.0"}});
