efe176359caa49347e359368973b6c0b85915d75
GitHub Action <actions@github.com>
2021-03-29 10:16:51 +0000

    removing user_changes from appsAway

--- user_changes/1617012890111-Fabrizio69-robots.js
+++
@@ -1,3 +0,0 @@
-//Fabrizio69
-//&<h3>Field changed in robot manager</h3><br><ul><li>ID: <ins>45</ins><ul><li>institution_name: <b>University of Zagreb, Zagreb</b></li><li>laboratory_name: <b>Faculty of Mechanical Engineering and Naval Architecture</b></li><li>laboratory_url: <b>https://www.fsb.unizg.hr/index.php?fsbonline_en&lang=en</b></li></ul></li></ul>
-db.robots.update ({id: 45},{$set: { institution_name: "University of Zagreb, Zagreb", laboratory_name: "Faculty of Mechanical Engineering and Naval Architecture", laboratory_url: "https://www.fsb.unizg.hr/index.php?fsbonline_en&lang=en"}});
