910c39b130f64443c10f5897f9d6c6d54b168a88
GitHub Action <actions@github.com>
2021-03-30 07:57:44 +0000

    removing user_changes from appsAway

--- user_changes/1617090674683-traversaro-robots.js
+++
@@ -1,3 +0,0 @@
-//traversaro
-//&<h3>Field changed in robot manager</h3><br><ul><li>ID: <ins>31</ins><ul><li>notes: <b>Robot equipped with a real battery, as opposed to other 2.5.5, 2.6 and 2.7 that have a fake battery.</b></li></ul></li></ul>
-db.robots.update ({id: 31},{$set: { notes: "Robot equipped with a real \"battery\", as opposed to other 2.5.5, 2.6 and 2.7 that have a fake battery."}});
