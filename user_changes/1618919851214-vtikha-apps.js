3690df36b0b922706d7d061d13f100a253c8ba97
GitHub Action <actions@github.com>
2021-04-20 12:00:48 +0000

    Merged user changes in appsAway

---
+++ user_changes/1618919851214-vtikha-apps.js
@@ -0,0 +1,3 @@
+//vtikha
+//&<h3>Field changed in app: <ins>Gazebo Simulation Deployment TEST</ins></h3><ul><li>title: Gazebo Simulation Deployment<b> TEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fede")},{$set: { title: "Gazebo Simulation Deployment TEST"}});
