e0ddcea352dfd8d38ad9c9197bc79e2cbcc8c420
GitHub Action <actions@github.com>
2021-05-04 12:02:12 +0000

    removing user_changes from appsAway

--- user_changes/1620129368630-ilaria-carlini-robots.js
+++
@@ -1,3 +0,0 @@
-//ilaria-carlini
-//&<h3>Field changed in robot: <ins>iCubBarcelona01</ins></h3><ul><li>custom: [<del>{title:test title,body:test body}</del>]</li></ul>
-db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee45")},{$set: { custom: []}});
