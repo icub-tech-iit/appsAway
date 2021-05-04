01058981fe96df9d121dc3ddd560f576b380289d
GitHub Action <actions@github.com>
2021-05-04 12:00:48 +0000

    Merged user changes in appsAway

---
+++ user_changes/1620129349324-ilaria-carlini-robots.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//&<h3>Field changed in robot: <ins>iCubAnkara</ins></h3><ul><li>custom: [<del>{title:TEST,body:Body test message}</del>]</li></ul>
+db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee49")},{$set: { custom: []}});
