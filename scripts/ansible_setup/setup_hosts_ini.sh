79595ebf74842ad43d8dad8eeff882d538ead98e
laura.cavaliere@iit.it <laura.cavaliere@iit.it>
2021-05-06 17:15:03 +0200

    Updated 'setup_hosts_ini.sh' and 'Makefile' according to #602

--- scripts/ansible_setup/setup_hosts_ini.sh
+++ scripts/ansible_setup/setup_hosts_ini.sh
@@ -9,123 +9,33 @@ fi
 
 echo "[nodes:children]" >> ./hosts.ini
 
-
-if [ ! -z "${APPSAWAY_CONSOLENODE_ADDR}" ]
-then
-    echo "console" >> ./hosts.ini
-fi
-
-if [ ! -z "${APPSAWAY_GUINODE_ADDR}" ]
-then
-    echo "gui" >> ./hosts.ini
-fi
-
-if [ ! -z "${APPSAWAY_ICUBHEADNODE_ADDR}" ]
-then
-    echo "head" >> ./hosts.ini
-fi
-
-if [ ! -z "${APPSAWAY_WORKERNODE_ADDR}" ]
-then
-    echo "worker" >> ./hosts.ini
-fi
-
-if [ ! -z "${APPSAWAY_CUDANODE_ADDR}" ]
-then
-    echo "cuda" >> ./hosts.ini
-fi
-
-if [ -z "${APPSAWAY_CONSOLENODE_ADDR}" ]
-then
-    echo "console" >> ./hosts.ini
-fi
-
+nodes_addr_array=($APPSAWAY_NODES_ADDR_LIST)
+nodes_usr_array=($APPSAWAY_NODES_USERNAME_LIST)
+nodes_name_array=($APPSAWAY_NODES_NAME_LIST)
+nodes_len=${#nodes_addr_array[@]}
+ 
+for (( i=0; i<$nodes_len; i++ )) 
+do  
+     echo "${nodes_name_array[$i]}" >> ./hosts.ini
+done 
 echo "" >> ./hosts.ini
 
 # now we populate each node
+for (( i=0; i<$nodes_len; i++ ))
+do  
+     echo "[${nodes_name_array[$i]}]
+${nodes_name_array[$i]}Laptop ansible_host=${nodes_addr_array[$i]}
 
-if [ ! -z "${APPSAWAY_CONSOLENODE_ADDR}" ]
-then
-    echo "[console]
-consoleLaptop ansible_host=${APPSAWAY_CONSOLENODE_ADDR}
+[${nodes_name_array[$i]}:vars]
+ansible_ssh_user=\"${nodes_usr_array[$i]}\"
+ansible_become_pass={{ ${nodes_name_array[$i]}_pass }}
 " >> ./hosts.ini
-fi
+done
 
-if [ ! -z "${APPSAWAY_GUINODE_ADDR}" ]
-then
-    echo "[gui]
-guiLaptop ansible_host=${APPSAWAY_GUINODE_ADDR}
-" >> ./hosts.ini
-fi
 
-if [ ! -z "${APPSAWAY_ICUBHEADNODE_ADDR}" ]
-then
-    echo "[head]
-headLaptop ansible_host=${APPSAWAY_ICUBHEADNODE_ADDR}
-" >> ./hosts.ini
-fi
 
-if [ ! -z "${APPSAWAY_WORKERNODE_ADDR}" ]
-then
-    echo "[worker]" >> ./hosts.ini
-    it_num=0
-    for IP in ${APPSAWAY_WORKERNODE_ADDR}
-    do
-        echo "workerLaptop$it_num ansible_host=$IP" >> ./hosts.ini
-        it_num=$(( $it_num+1 ))
-    done
-    echo "" >> ./hosts.ini
-fi
 
-if [ ! -z "${APPSAWAY_CUDANODE_ADDR}" ]
-then
-    echo "[cuda]" >> ./hosts.ini
-    it_num=0
-    for IP in ${APPSAWAY_CUDANODE_ADDR}
-    do
-        echo "workerLaptop$it_num ansible_host=$IP" >> ./hosts.ini
-        it_num=$(( $it_num+1 ))
-    done
-    echo "" >> ./hosts.ini
-fi
 
-if [ -z "${APPSAWAY_USER_NAME}" ]
-then
-    APPSAWAY_USER_NAME=${USER}
-fi
 
-if [ -z "${APPSAWAY_CONSOLENODE_ADDR}" ]
-then
-    if [ "$os" = "Darwin" ]
-    then
-        LOCALHOST=$(arp -a | awk -F'[()]' '{print $2}')
-    else
-        LOCALHOST=$(hostname -I | awk '{print $1}')
-    fi
-    
-    echo "[console]
-    consoleLaptop ansible_host=${LOCALHOST}
-    " >> ./hosts.ini
-fi
 
-APPSAWAY_USER_PASSWORD=$1
-
-if [ -z "${APPSAWAY_USER_PASSWORD}" ]
-then
-   
-    echo "Your username is ${APPSAWAY_USER_NAME}"
-    read -p "Insert your password " -s PASSWORD
-    
-    APPSAWAY_USER_PASSWORD=${PASSWORD}
-
-    echo "[all:vars]
-    ansible_ssh_user=\"${APPSAWAY_USER_NAME}\"
-    ansible_ssh_pass=\"${APPSAWAY_USER_PASSWORD}\" 
-    ansible_become_pass=\"${APPSAWAY_USER_PASSWORD}\"" >> ./hosts.ini
-
-else
-    echo "[all:vars]
-    ansible_ssh_user=\"${APPSAWAY_USER_NAME}\"
-    ansible_become_pass=\"${APPSAWAY_USER_PASSWORD}\" " >> ./hosts.ini
-fi
 
