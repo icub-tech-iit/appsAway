dee5e5f36756b272fbe9aed3f80263cef15c6ae0
GitHub Action <actions@github.com>
2021-06-01 08:55:44 +0000

    Merged user changes in appsAway

---
+++ user_changes/1622537486095-ilaria-carlini-robots.js
@@ -0,0 +1,3 @@
+//ilaria-carlini
+//&<h3>Field changed in robot: <ins>iCubAberystwyth01</ins></h3><ul><li>img: https://storage.googleapis.com/robot-bazaar-files/robots/i<del>c</del><b>C</b>ub<del>_d</del><b>Ab</b>e<del>fa</del><b>rystwyth01/1622537486095-iC</b>u<del>l</del><b>bAberys</b>t<b>wyth01-img</b>.png</li></ul>
+db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee4b")},{$set: { img: "https://storage.googleapis.com/robot-bazaar-files/robots/iCubAberystwyth01/1622537486095-iCubAberystwyth01-img.png"}});
