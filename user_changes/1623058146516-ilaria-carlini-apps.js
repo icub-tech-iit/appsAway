2ced46b05ead517fd7f52b3caac8eaab8f01a030
GitHub Action <actions@github.com>
2021-06-07 09:30:52 +0000

    Merged user changes in appsAway

---
+++ user_changes/1623058146516-ilaria-carlini-apps.js
@@ -0,0 +1,4 @@
+//ilaria-carlini
+//
+//&<h3>Field changed in app: <ins>Face and Gaze Tracking</ins></h3><ul><li>description: Deploy the Face and Gaze Tracking application on your cluster<b> TEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fee9")},{$set: { description: "Deploy the Face and Gaze Tracking application on your cluster TEST"}});
