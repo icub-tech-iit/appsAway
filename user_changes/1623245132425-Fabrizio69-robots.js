44d294d967c4aac6a543e88623a0031ffbb62178
GitHub Action <actions@github.com>
2021-06-09 13:31:56 +0000

    removing user_changes from appsAway

--- user_changes/1623245132425-Fabrizio69-robots.js
+++
@@ -1,4 +0,0 @@
-//Fabrizio69
-//
-//&<h3>Field changed in robot manager</h3><br><ul><li>ID: <ins>46</ins><ul><li>private: <del>tru</del><b>fals</b>e</li></ul></li></ul>
-db.robots.update ({id: 46},{$set: { private: "false"}});
