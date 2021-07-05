8af4b2a88ed734e23b302ffc43a98e727d100504
vvasco <valentina.vasco@iit.it>
2021-07-05 11:58:51 +0200

    [yarpBasicDeploy] fixed image names and added gui option inside test-script

--- demos/yarpBasicDeploy/test-script.sh
+++ demos/yarpBasicDeploy/test-script.sh
@@ -106,6 +106,10 @@ echo "tags: $APPSAWAY_TAGS"
 
 echo "about to setup the cluster..." 
 ./appsAway_setupCluster.sh
+
+echo "
+LOCAL_IMAGE_FLAG=false" >> $HOME/iCubApps/$APPSAWAY_APP_NAME/.env 
+
 ./appsAway_setupSwarm.sh
 setupEnvironment
 ./appsAway_copyFiles.sh
