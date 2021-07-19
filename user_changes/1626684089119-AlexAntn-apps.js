c9000bbf6038b6a881f9b94cd4a8f63b408e7029
GitHub Action <actions@github.com>
2021-07-19 09:53:51 +0000

    Merged user changes in appsAway

---
+++ user_changes/1626684089119-AlexAntn-apps.js
@@ -0,0 +1,4 @@
+//AlexAntn
+//
+//&<h3>Field changed in app: <ins>Gazebo Simulation Deployment</ins></h3><ul><li>description: Deploy a pre configured Docker including Gazebo simulator for your system<b>. THIS IS A TEST</b></li></ul>
+db.apps.update ({_id: ObjectId("5f4375f118a3dc019958fede")},{$set: { description: "Deploy a pre configured Docker including Gazebo simulator for your system. THIS IS A TEST"}});
