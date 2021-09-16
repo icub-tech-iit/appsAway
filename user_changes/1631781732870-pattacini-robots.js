3ff1095caf695e4987eab2fc6c62d8e1dab42d33
GitHub Action <actions@github.com>
2021-09-16 08:47:15 +0000

    removing user_changes from appsAway

--- user_changes/1631781732870-pattacini-robots.js
+++
@@ -1,4 +0,0 @@
-//pattacini
-//
-//&<h3>Field changed in robot: <ins>iCubNancy01</ins></h3><ul><li>report: [<b>{filename:Report of iCubNancy01,url:https://github.com/robotology/robots-configuration/releases/download/v1.21.1/iCubNancy01_report.html}</b>]</li></ul>
-db.robots.update ({_id: ObjectId("5fdbf1db9d8a3bc1ead6ee56")},{$set: { report: [{"filename":"Report of iCubNancy01","url":"https://github.com/robotology/robots-configuration/releases/download/v1.21.1/iCubNancy01_report.html"}]}});
