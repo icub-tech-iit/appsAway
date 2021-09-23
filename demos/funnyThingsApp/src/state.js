6990026d84bdbd587c2b22d66f86cc6249a0436f
GrmanRodriguez <grmanrod@gmail.com>
2021-09-23 10:48:43 +0200

    added explanatory comments

--- demos/funnyThingsApp/src/state.js
+++ demos/funnyThingsApp/src/state.js
@@ -48,36 +48,36 @@ var changeActivityValue = (activityIndex, optionIndex, value) => {
 }
 
 var checkActivites = (activities) => {
-    if (!Array.isArray(activities)) return false;
+    if (!Array.isArray(activities)) return false; // Activities should be an array
     if (activities.length < 1) return false; // There should be at least one activity
     for (let activity of activities) {
-        if (!ACTIONS.hasOwnProperty(activity.activity)) return false;
+        if (!ACTIONS.hasOwnProperty(activity.activity)) return false; // Activity should be part of ACTIONS
         let possibleOptionValues = OPTIONS[activity.activity];
         if (Array.isArray(possibleOptionValues)) {
             for (let optionTemplate of possibleOptionValues) {
-                if (!Array.isArray(activity.options)) return false;
+                if (!Array.isArray(activity.options)) return false; // OPtions for activity should be an array
                 let importedOption = activity.options.filter(option => option.label == optionTemplate.label)
                 if (importedOption.length !== 1) return false; // There should be exactly one option with the same label as the template
                 importedOption = importedOption[0]
                 let optionType = optionTemplate.type;
                 switch (optionType) {
                     case "string":
-                        if (typeof importedOption.value !== "string") return false;
+                        if (typeof importedOption.value !== "string") return false; // Value should be a string
                         break;
                     case "float":
                         let floatRegex = new RegExp("^-?\\d*(\\.\\d+)?$")
-                        if (!floatRegex.test(importedOption.value)) return false;
+                        if (!floatRegex.test(importedOption.value)) return false; // Value should be parseable to float
                         break;
                     case "select":
                     case "dropdown":
-                        if (!optionTemplate.options.some(optionValue => optionValue == importedOption.value)) return false;
+                        if (!optionTemplate.options.some(optionValue => optionValue == importedOption.value)) return false; // Value should be part of the options
                         break;
                     default:
                         return false;
                 }
             }
         } else if(activity.hasOwnProperty("options")) {
-            return false;
+            return false; // if ACTION has no options the activity should not have options either
         }
     }
     return true;
