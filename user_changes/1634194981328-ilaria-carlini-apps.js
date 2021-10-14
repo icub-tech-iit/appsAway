4f875143637c40c87cef16abda238a0d768b630a
GitHub Action <actions@github.com>
2021-10-14 07:46:03 +0000

    removing user_changes from appsAway

--- user_changes/1634194981328-ilaria-carlini-apps.js
+++
@@ -1,4 +0,0 @@
-//ilaria-carlini
-//
-//&<h3>Field changed in app: <ins>Supervised Calibration Application</ins></h3><ul><li>updated: <del>Sep</del><b>Oct</b> 2021</li></ul>
-db.apps.update ({_id: ObjectId("600965c3f083227d6a2fb23e")},{$set: { updated: "Oct 2021"}});
