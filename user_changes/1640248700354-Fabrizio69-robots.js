acece7855dc34b06d7c432da10540c2aaab372aa
GitHub Action <actions@github.com>
2021-12-23 09:05:07 +0000

    removing user_changes from appsAway

--- user_changes/1640248700354-Fabrizio69-robots.js
+++
@@ -1,4 +0,0 @@
-//Fabrizio69
-//
-//&<h3>Field changed in robot: <ins>iCubValparaiso01</ins></h3><ul><li>report: [<b>{filename:Configuration report of the SN048 iCubValparaiso01 - Release Version v1.22.1,url:https://github.com/robotology/robots-configuration/releases/download/v1.22.1/iCubValparaiso01_report.html}</b>]</li></ul>
-db.robots.update ({_id: ObjectId("6194dadcb0d8ce5b8d98a38a")},{$set: { report: [{"filename":"Configuration report of the SN048 iCubValparaiso01 - Release Version v1.22.1","url":"https://github.com/robotology/robots-configuration/releases/download/v1.22.1/iCubValparaiso01_report.html"}]}});
