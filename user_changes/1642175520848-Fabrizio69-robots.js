388aa0c5ba076fd7cb5d5b27b691664f7166128a
GitHub Action <actions@github.com>
2022-01-14 15:57:21 +0000

    removing user_changes from appsAway

--- user_changes/1642175520848-Fabrizio69-robots.js
+++
@@ -1,4 +0,0 @@
-//Fabrizio69
-//
-//&<h3>Field changed in robot manager</h3><br><ul><li>ID: <ins>2</ins><ul><li>sensors: [/dev/<del>bo</del><b>ttyX</b>s<del>ch-i2c-imu</del><b>ens</b>]</li></ul></li></ul>
-db.robots.update ({id: 2},{$set: { sensors: ["/dev/ttyXsens"]}});
