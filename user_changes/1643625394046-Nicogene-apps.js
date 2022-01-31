3e0cc35c113fbc525160c815ee419617fe3251cb
GitHub Action <actions@github.com>
2022-01-31 14:27:08 +0000

    removing user_changes from appsAway

--- user_changes/1643625394046-Nicogene-apps.js
+++
@@ -1,4 +0,0 @@
-//Nicogene
-//
-//&<h3>Field changed in app: <ins>Blender controller</ins></h3><ul><li>complexity: <del>0</del><b>1</b></li><li>laboratories: [<b>{name:iCub Tech,url:https://www.iit.it/it/web/icub-tech}</b>]</li></ul>
-db.apps.update ({_id: ObjectId("61f3e7adf8f1e18089051cf0")},{$set: { complexity: "1",laboratories: [{"name":"iCub Tech","url":"https://www.iit.it/it/web/icub-tech"}]}});
