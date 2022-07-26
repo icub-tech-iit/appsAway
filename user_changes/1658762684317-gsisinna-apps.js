5229fcdc9e2588a82706667b4c93a386d5c6ee38
GitHub Action <actions@github.com>
2022-07-26 12:23:23 +0000

    removing user_changes from appsAway

--- user_changes/1658762684317-gsisinna-apps.js
+++
@@ -1,4 +0,0 @@
-//gsisinna
-//
-//&<h3>Field changed in app: <ins>iCubTest</ins></h3><ul><li>dependencies: [icub-head<del>,icub-head</del>]</li></ul>
-db.apps.update ({_id: ObjectId("62d11e87f4879ffd832a2b1f")},{$set: { dependencies: ["icub-head"]}});
